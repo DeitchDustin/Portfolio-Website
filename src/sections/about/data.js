ab39fdc8bbd0e0d382415ea61e569e6de7f6e45f
Dustin Deitch <DeitchDustin@gmail.com>
2023-04-05 10:54:32 -0700

    added data for about card

---
+++ src/sections/about/data.js
@@ -0,0 +1,11 @@
+import {FaAward} from 'react-icons/fa';
+import {TbBooks} from 'react-icons/tb';
+//import {BiHappyHeartEyes} from 'react-icons/bi';
+
+const data = [
+  {id: 1, icon: <FaAward/>, title: 'Experience', desc: ''},
+  {id: 2, icon: <TbBooks/>, title: 'Projects', desc: ''},
+  //{id: 3, icon: <BiHappyHeartEyes/>, title: '', desc: ''},
+]
+
+export default data;
