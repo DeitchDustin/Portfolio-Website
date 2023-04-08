685925bd91ad5f4600c5a6d377b1650b738a0229
Dustin Deitch <DeitchDustin@gmail.com>
2023-04-08 12:32:06 -0700

    Working on Portfolio section

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
 import './portfolio.css';
+import ProjectCatagories from './ProjectCatagories';
+import Projects from './Projects';
+import data from './data';
+import { useState } from 'react'
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState(data);
+
   return (
     <section id="portfolio">
-      <h2>Portfolio</h2>
+      <h2>Recent Projects</h2>
+      <p>
+        Check out the projects that I've been working on.
+      </p>
+      <div className='container portfolio__container'>
+        <ProjectCatagories/>
+        <Projects projects={projects}/>
+      </div>
     </section>
   )
 }
---
+++ src/sections/portfolio/ProjectCatagories.jsx
@@ -0,0 +1,9 @@
+
+
+const ProjectCatagories = () => {
+  return (
+      <div>ProjectCatagories</div>
+  )
+}
+
+export default ProjectCatagories;
