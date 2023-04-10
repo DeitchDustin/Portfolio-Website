070b9b6d47429c22c1d7bbf530c6bdb5d4b1f18c
Dustin Deitch <DeitchDustin@gmail.com>
2023-04-10 13:42:48 -0700

    complete floating navbar

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,11 +1,18 @@
-import './floating-nav.css';
+import data from './data'
+import Scrollspy from 'react-scrollspy'
+import Nav from './Nav'
+import './floating-nav.css'
 
 const FloatingNav = () => {
   return (
-    <section id="floating-nav">
-      
-    </section>
+    <ul id="floating__nav">
+      <Scrollspy offset={-500} className='scrollspy' items={['header', 'about', 'portfolio', 'contact']} currentClassName="active">
+        {
+          data.map(item => <Nav key={item.id} item={item} />)
+        }
+      </Scrollspy>
+    </ul>
   )
 }
 
-export default FloatingNav;
+export default FloatingNav
